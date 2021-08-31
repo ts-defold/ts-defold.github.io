6fb458e70ace3a6f3bbcc3bf0c48b7eb230b62be
Justin Walsh <contact.me@thejustinwalsh.com>
2021-08-31 00:58:19 -0700

    fix: play reload animation once - demonstartes hot reload better

--- src/playground/snippets/go-sprite.js
+++ src/playground/snippets/go-sprite.js
@@ -9,18 +9,18 @@ type Props = {
 export function init(this: Props) {
   this.key = "position.y";
   this.distance = 100;
-  this.speed = 2;
+  this.speed = 1;
   this.theta = 0;
 }
 
 export function update(this: Props, dt: number) {
-  this.theta += this.speed / 2 * dt;
+  this.theta += this.speed * dt;
   go.set_position(vmath.vector3(Math.cos(this.theta) * 200, 0, 0));
 }
 
 export function on_reload(this: Props) {
   go.set_position(vmath.vector3(0, -this.distance, 0));
   go.cancel_animations(".", this.key);
-  go.animate(".", this.key, go.PLAYBACK_LOOP_PINGPONG, this.distance, go.EASING_INOUTELASTIC, this.speed);
+  go.animate(".", this.key, go.PLAYBACK_ONCE_PINGPONG, this.distance, go.EASING_INOUTELASTIC, this.speed);
 }
 `
